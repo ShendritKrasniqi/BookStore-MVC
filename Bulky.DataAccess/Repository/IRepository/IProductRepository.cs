f52e6a909a4eb77bfabd07f300a584351c48d9de
ShendritKrasniqi <shendritkrasniqi02@gmail.com>
2024-03-28 21:10:03 +0100

    Product CRUD

--- Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using BulkyBook.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +7,8 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository.IRepository
 {
-    public interface IUnitOfWork
+    public interface IProductRepository : IRepository<Product>
     {
-        ICategoryRepository Category { get; }
-        IProductRepository Product { get; }
-
-        void Save();
+        void Update(Product obj);
     }
 }
